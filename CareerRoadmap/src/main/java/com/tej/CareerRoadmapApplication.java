a82966fffb261244bd62a1a95a8cc8fe1e89dc39
Tejas <tejasbedke@gmail.com>
2024-12-26 10:19:12 +0530

    Initial Commit

---
+++ CareerRoadmap/src/main/java/com/tej/CareerRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.tej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerRoadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerRoadmapApplication.class, args);
+	}
+
+}
